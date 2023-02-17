f438b4cc180c77997ed46041d9a68fc2c2664b73
MercyWhistles <82103220+ErisWhistles@users.noreply.github.com>
2023-02-17 11:20:30 -0500

    changes

--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?= $data ?></title>
-	<link rel="stylesheet" type="text/css" href="/CSS/default.css">
+	<link rel="stylesheet" type="text/css" href="/resources/styles/default.css">
 	
 </head>
 <body>
--- app/views/shared/navBar.php
+++ app/views/shared/navBar.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="/CSS/navBar.css">
+<link rel="stylesheet" type="text/css" href="resources/styles/navBar.css">
 
 <div class="bar">
     <div class="ls1">
