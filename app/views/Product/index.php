d5cfd73dd53bd7c869e85b54172b52590d8bb72a
MercyWhistles <82103220+ErisWhistles@users.noreply.github.com>
2023-02-19 17:51:52 -0500

    Product Page Changes

--- app/views/Product/index.php
+++ app/views/Product/index.php
@@ -1,10 +1,15 @@
 <?php $this->view('shared/header','Main Index'); ?>
 <?php $this->view('shared/navBar'); ?>
+<link rel="stylesheet" type="text/css" href="/resources/styles/product_page.css">
 
 <!-- from here is still in a div Wrapper, div Wrapper start in header.php and close in footer.php. --> 
 
 <div class="body">
-    <h1>Product Page!!</h1>
+    <div class="product_grid">
+        <div class="product_box">
+
+        </div>
+    </div>
 </div>
 
 <?php $this->view('shared/footer'); ?>
--- app/views/shared/navBar.php
+++ app/views/shared/navBar.php
@@ -3,11 +3,11 @@
 <div class="bar">
     <div class="ls1">
         <a href="/Main/index">Home</a> <!-- this = HomePage -->
-        <a href="/Product/index">Product</a> <!-- not yet created -->
+        <a href="/Product/index">Products</a> <!-- not yet created -->
     </div>
     <div class="ellipse"></div>
     <div class="ls2">
-        <a href="/Main/about_us">About</a> <!-- not yet created -->
+        <a href="/Main/about_us">About Us</a> <!-- not yet created -->
         <a href="/Contact/index">Contact Us</a> <!-- not yet created -->
     </div>
 </div>
