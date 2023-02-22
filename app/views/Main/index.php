0ef27d79244622421db605ae0312d8613abdb79e
MercyWhistles <82103220+ErisWhistles@users.noreply.github.com>
2023-02-22 14:32:58 -0500

    bootstrap added with carousel

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,10 +1,35 @@
 <?php $this->view('shared/header','Main Index'); ?>
 <?php $this->view('shared/navBar'); ?>
 
+<link rel="stylesheet" href="/resources/styles/bootstrap.css">
+<link rel="stylesheet" href="/resources/styles/home.css">
+
+
 <!-- from here is still in a div Wrapper, div Wrapper start in header.php and close in footer.php. --> 
 
 <div class="body">
-	<h1>Home Page!!!</h1>
+	<div class="carouselSize">
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/resources/images/bunny.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/resources/images/bunny2.webp" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
+</div>
+</div>
+
+<script src="/resources/scripts/bootstrap.js"></script>
 
 <?php $this->view('shared/footer'); ?>
