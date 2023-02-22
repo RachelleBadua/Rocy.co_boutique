1f8af4f99c3a38ffb9091f4ba67082596f6d8f51
MercyWhistles <82103220+ErisWhistles@users.noreply.github.com>
2023-02-22 15:59:53 -0500

    Product Grid Implemented

--- app/views/Product/index.php
+++ app/views/Product/index.php
@@ -1,10 +1,9 @@
 <?php $this->view('shared/header','Main Index'); ?>
-
-<!-- <link rel="stylesheet" type="text/css" href="/resources/styles/product_page.css"> -->
+<link rel="stylesheet" type="text/css" href="/resources/styles/product_page.css">
 
 <body>
 <?php $this->view('shared/navBar'); ?>
-	<div>
+	<div class="product_grid">
         <p>Hi</p>
     </div>
 </body>
