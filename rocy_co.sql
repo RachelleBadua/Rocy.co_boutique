7e4f7e9291016e89fd14a11eaf479a43f43aaef1
DokuSan <92054325+DokuSan00@users.noreply.github.com>
2023-04-23 22:58:06 -0400

    3 categories added, 2 products added. Hard Coded. new DB user account added

--- rocy_co.sql
+++ rocy_co.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2023 at 04:56 AM
+-- Generation Time: Apr 24, 2023 at 04:57 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
