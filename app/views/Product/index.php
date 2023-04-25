bde5f37b7514e049392b23e43689d423679912d2
DokuSan <92054325+DokuSan00@users.noreply.github.com>
2023-04-25 15:31:13 -0400

    ..

--- app/views/Product/index.php
+++ app/views/Product/index.php
@@ -4,15 +4,46 @@
 
 <body>
 <?php $this->view('shared/navBar'); ?>
+<h1>Catalog</h1>
 	<div class="product_grid">
         <div class="sideBar">
             <p>sideBar</p>
         </div>
         <div class="vl"></div>
         <div class="products">
-            <p>products</p>
-            <?= var_dump($data) ?>
+            <div class="grid-container">
+                <?php foreach($data as $product) {?>
+                    <div class="grid-item">
+                        <?php 
+                            echo "<img src='/resources/productImages/$product->image'>";
+                            echo "<h3>".ucwords($product->product_name)."<h3>";
+                            // echo "<h4 class='price'>\$$product->sellingPrice";
+                        ?>
+                    </div>
+                <?php } ?>
+                <?php foreach($data as $product) {?>
+                    <div class="grid-item">
+                        <?php 
+                            echo "<img src='/resources/productImages/$product->image'>";
+                            echo "<h3>".ucwords($product->product_name)."<h3>";
+                            // echo "<h4 class='price'>\$$product->sellingPrice";
+                        ?>
+                    </div>
+                <?php } ?>
+                <?php foreach($data as $product) {?>
+                    <div class="grid-item">
+                        <?php 
+                            echo "<img src='/resources/productImages/$product->image'>";
+                            echo "<h3>".ucwords($product->product_name)."<h3>";
+                            // echo "<h4 class='price'>\$$product->sellingPrice";
+                        ?>
+                    </div>
+                <?php } ?>
+            </div>
         </div>
     </div>
 </body>
     
+<?php 
+
+?>
