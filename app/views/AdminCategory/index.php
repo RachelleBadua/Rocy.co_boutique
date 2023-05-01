f49c4f1574063e8b28af707b6079276f451e178a
RachelleBadua <RcahelleBadua04@outlook.com>
2023-05-01 02:04:09 -0400

    have to fix dropdown list in admin category page

--- app/views/AdminCategory/index.php
+++ app/views/AdminCategory/index.php
@@ -18,15 +18,12 @@
 					<?php foreach ($data as $category) { ?>
 					<tr>
 
-						<!-- <td>< htmlentities($product->product_id) ?></td> -->
-						<!-- <td><a href='/AdminProduct/productDetails/<?=$category->category_id?>'><?= htmlentities($category->category_id) ?></a></td> -->
+						<!-- <?= htmlentities($category->category_id) ?></a></td>  -->
 						<td><?= htmlentities($category->category_id) ?></td>
 						<td><?= htmlentities($category->category) ?></td>
 						<td>
 							<!-- TODO: make function to delete product-->
 							<a href='/AdminCategory/delete/<?=$category->category_id?>'><?= _('remove') ?></a> 
-							<!-- TODO: make function to edit product -->
-							
 						</td>
 					</tr>
 					<?php
@@ -36,10 +33,31 @@
 		</div>
 		<div class="addEdit">
 			<div class="addCategory">
-				hi miso
+				<h4>Add Category</h4>
+				<form class="form-addCategory" method="post" action="" enctype="multipart/form-data" style="">
+					<div class="btn-addCategory">
+						<input type="text" name="category">
+						<input type="submit" name="addAction" value="Add">
+					</div>
+				</form>
 			</div>
 			<div class="editCategory">
-				hi nugget
+				<h4>Edit Category</h4>
+				<form class="form-editCategory" method="post" enctype="multipart/form-data" action="">
+					<div class="btn-editCategory">
+						<select class="btn-input">
+							<?php
+								foreach($data as $category){
+									echo "<option value='$category->category_id'>$category->category_id</option>\n";
+								}
+
+							?>
+							
+						</select>
+						<input class="btn-input" type="text" name="category" value="<?= $category->category?>">
+						<input type="submit" name="editAction" value="Edit">
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
