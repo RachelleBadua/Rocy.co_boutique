58ab67372f65691debc83237f4a4416195e760f6
DokuSan <92054325+DokuSan00@users.noreply.github.com>
2023-05-07 06:09:05 -0400

    Myaccount page built, will conitnue on its pages. pageTitle min-width set -> prevent breaking the symmetry

--- app/views/MyAccount/index.php
+++ app/views/MyAccount/index.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" type="text/css" href="/resources/styles/pageContent.css">
+<link rel="stylesheet" type="text/css" href="/resources/styles/myAccount.css">
 
 <?php $this->view('shared/header','Main Index');?>
 
@@ -6,15 +7,49 @@
 
 <?php $this->view('shared/navBar'); ?>
 
-<h1 class='pageTitle'>My Account</h1>
-<div class='content'>
-    <h2>My Orders</h2>
-    <h2>Profile&Security</h2>
-    <h2>Shipping Address</h2>
+<h1 class='pageTitle'><?=_('My Account')?></h1>
+<div class='content'>   
+    <a href="">
+        <div class='button'>
+            <!-- <div class='icon'> -->
+                <img src="../resources/images/myOrders.png">
+            <!-- </div> -->
+            <div class='label'>
+                <h2><?=_('My Orders')?></h2>
+                <label><?=_("View all orders and track order's status")?></label>
+            </div>
+        </div>
+    </a>
+    
+    <a href="">
+        <div class='button'>
+            <!-- <div class='icon'> -->
+                <img src="../resources/images/security.png">
+            <!-- </div> -->
+            <div class='label'>
+                <h2><?=_('Profile & Security')?></h2>
+                <label><?=_("Manage profile and security's information")?></label>
+            </div>
+        </div>
+    </a>
+
+    <a href="">
+        <div class='button'>
+            <!-- <div class='icon'> -->
+                <img src="../resources/images/shipAddress.png">
+            <!-- </div> -->
+            <div class='label'>
+                <h2><?=_('Shipping Address')?></h2>
+                <label><?=_('Edit shipping address')?></label>
+            </div>
+        </div>
+    </a>
+    
     <div class="signout">
-        <a href="/User/logout">Sign out</a>
+        <a href="/User/logout"><button>Sign out</button></a>
     </div>
 </div>
 </div>
 <!-- </body> -->
 
+<?php $this->view('shared/footer'); ?>
