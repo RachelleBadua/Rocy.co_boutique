482c1ce5581ee1c4bc605daab88376d737e5244d
DokuSan <92054325+DokuSan00@users.noreply.github.com>
2023-05-09 14:27:28 -0400

    new SQL

--- rocy_co.sql
+++ rocy_co.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2023 at 06:30 AM
+-- Generation Time: May 09, 2023 at 08:26 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -30,11 +30,12 @@ USE `rocy_co`;
 --
 
 DROP TABLE IF EXISTS `about_us`;
-CREATE TABLE `about_us` (
-  `about_us_Id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `about_us` (
+  `about_us_Id` int(11) NOT NULL AUTO_INCREMENT,
   `image` varchar(72) NOT NULL,
-  `text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `text` text NOT NULL,
+  PRIMARY KEY (`about_us_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `about_us`
@@ -50,10 +51,11 @@ INSERT INTO `about_us` (`about_us_Id`, `image`, `text`) VALUES
 --
 
 DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category` (
-  `category_id` int(2) NOT NULL,
-  `category` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int(2) NOT NULL AUTO_INCREMENT,
+  `category` varchar(50) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `category`
@@ -72,13 +74,16 @@ INSERT INTO `category` (`category_id`, `category`) VALUES
 --
 
 DROP TABLE IF EXISTS `detail`;
-CREATE TABLE `detail` (
-  `detail_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `detail` (
+  `detail_id` int(5) NOT NULL AUTO_INCREMENT,
   `order_id` int(5) NOT NULL,
   `product_id` int(5) NOT NULL,
   `unit_price` float NOT NULL,
-  `quantity` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `quantity` int(3) NOT NULL,
+  PRIMARY KEY (`detail_id`),
+  KEY `order_id_detail_to_order` (`order_id`),
+  KEY `product_id_detail_to_product` (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `detail`
@@ -102,14 +107,15 @@ INSERT INTO `detail` (`detail_id`, `order_id`, `product_id`, `unit_price`, `quan
 --
 
 DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
-  `order_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `order` (
+  `order_id` int(5) NOT NULL AUTO_INCREMENT,
   `user_id` int(4) NOT NULL,
   `order_date` date NOT NULL DEFAULT current_timestamp(),
   `status` enum('cart','ordered','finished') DEFAULT NULL,
   `total_price` float NOT NULL,
-  `isDelivery` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `isDelivery` tinyint(1) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `order`
@@ -133,15 +139,17 @@ INSERT INTO `order` (`order_id`, `user_id`, `order_date`, `status`, `total_price
 --
 
 DROP TABLE IF EXISTS `product`;
-CREATE TABLE `product` (
-  `product_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `product` (
+  `product_id` int(5) NOT NULL AUTO_INCREMENT,
   `category_id` int(2) NOT NULL,
   `product_name` varchar(100) NOT NULL,
   `image` varchar(72) NOT NULL,
   `sellingPrice` float NOT NULL,
   `description` varchar(128) NOT NULL,
-  `quantity` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `quantity` int(3) NOT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `category_id_product_to_category` (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `product`
@@ -161,7 +169,7 @@ INSERT INTO `product` (`product_id`, `category_id`, `product_name`, `image`, `se
 --
 
 DROP TABLE IF EXISTS `profile`;
-CREATE TABLE `profile` (
+CREATE TABLE IF NOT EXISTS `profile` (
   `user_id` int(4) NOT NULL,
   `subscription` tinyint(1) NOT NULL,
   `name` varchar(50) NOT NULL,
@@ -169,7 +177,8 @@ CREATE TABLE `profile` (
   `address` varchar(50) NOT NULL,
   `city` varchar(50) NOT NULL,
   `province` varchar(50) NOT NULL,
-  `postal` varchar(7) NOT NULL
+  `postal` varchar(7) NOT NULL,
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -188,12 +197,13 @@ INSERT INTO `profile` (`user_id`, `subscription`, `name`, `phoneNo`, `address`, 
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `user_id` int(4) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(4) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `password` varchar(72) NOT NULL,
-  `roleType` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `roleType` varchar(10) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
@@ -204,96 +214,7 @@ INSERT INTO `user` (`user_id`, `email`, `password`, `roleType`) VALUES
 (6, 'TestUser2', '$2y$10$litEQ/Cl5hIZPyAWFAc/DOXUOuiAB53LlpFrD63VtLX7CCwrN6unK', 'customer'),
 (9, 'TestUser4', '$2y$10$8tEuaTguZitcslQwBkaVDus8nrMq921J4kdD7YKl.8BgsW2xbnKGy', 'customer'),
 (10, 'abc@abc.com', '$2y$10$kBfo8N.XaTnCdQsGJIEcxuecaLl54tzYxQFo5cj/.GkqLrp1qrDky', 'customer'),
-(11, 'eto@eto.com', '$2y$10$DCLqcAKIsJGM.aml1f33Ue7cIZy3m9xNNXuGsYxg8jgJyZrMFfbUK', 'customer');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `about_us`
---
-ALTER TABLE `about_us`
-  ADD PRIMARY KEY (`about_us_Id`);
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`category_id`);
-
---
--- Indexes for table `detail`
---
-ALTER TABLE `detail`
-  ADD PRIMARY KEY (`detail_id`),
-  ADD KEY `order_id_detail_to_order` (`order_id`),
-  ADD KEY `product_id_detail_to_product` (`product_id`);
-
---
--- Indexes for table `order`
---
-ALTER TABLE `order`
-  ADD PRIMARY KEY (`order_id`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`),
-  ADD KEY `category_id_product_to_category` (`category_id`);
-
---
--- Indexes for table `profile`
---
-ALTER TABLE `profile`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `about_us`
---
-ALTER TABLE `about_us`
-  MODIFY `about_us_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `category_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `detail`
---
-ALTER TABLE `detail`
-  MODIFY `detail_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT for table `order`
---
-ALTER TABLE `order`
-  MODIFY `order_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `product_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+(11, 'eto@eto.com', '$2y$10$DCLqcAKIsJGM.aml1f33Ue7cIZy3m9xNNXuGsYxg8jgJyZrMFfbUK', 'admin');
 
 --
 -- Constraints for dumped tables
