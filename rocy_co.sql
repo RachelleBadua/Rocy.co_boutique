ba1ed9337c216ad2401e533a2588305737dcb1f1
Rachelle Badua <2131305@edu.vaniercollege.qc.ca>
2023-05-10 18:32:43 -0400

    updated sql

--- rocy_co.sql
+++ rocy_co.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2023 at 03:16 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: May 11, 2023 at 12:31 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,14 +34,14 @@ CREATE TABLE `about_us` (
   `about_us_Id` int(11) NOT NULL,
   `image` varchar(72) NOT NULL,
   `text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `about_us`
 --
 
 INSERT INTO `about_us` (`about_us_Id`, `image`, `text`) VALUES
-(1, '645869acc9dba.png', 'The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn\'t distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.\r\n\r\nThe passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it\'s seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.');
+(1, '645ba640c2133.png', 'The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn&#039;t distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.\r\n\r\nThe passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again ');
 
 -- --------------------------------------------------------
 
@@ -53,7 +53,7 @@ DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `category_id` int(2) NOT NULL,
   `category` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `category`
@@ -78,7 +78,19 @@ CREATE TABLE `detail` (
   `product_id` int(5) NOT NULL,
   `unit_price` float NOT NULL,
   `quantity` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detail`
+--
+
+INSERT INTO `detail` (`detail_id`, `order_id`, `product_id`, `unit_price`, `quantity`) VALUES
+(20, 17, 1, 5.99, 3),
+(21, 19, 1, 5.99, 5),
+(23, 21, 14, 4.99, 1),
+(24, 21, 2, 3.99, 3),
+(25, 18, 15, 12.99, 3),
+(26, 18, 11, 5.99, 1);
 
 -- --------------------------------------------------------
 
@@ -94,7 +106,20 @@ CREATE TABLE `order` (
   `status` enum('cart','ordered','finished') DEFAULT NULL,
   `total_price` float NOT NULL,
   `isDelivery` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`order_id`, `user_id`, `order_date`, `status`, `total_price`, `isDelivery`) VALUES
+(17, 12, '2023-05-10', 'ordered', 17.97, 1),
+(18, 12, '2023-05-10', 'ordered', 44.96, 1),
+(19, 13, '2023-05-10', 'ordered', 29.95, 1),
+(20, 13, '2023-05-10', 'cart', 0, 0),
+(21, 14, '2023-05-10', 'ordered', 16.96, 1),
+(22, 14, '2023-05-10', 'cart', 0, 0),
+(23, 12, '2023-05-10', 'cart', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -111,7 +136,7 @@ CREATE TABLE `product` (
   `sellingPrice` float NOT NULL,
   `description` varchar(128) NOT NULL,
   `quantity` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `product`
@@ -120,7 +145,9 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`product_id`, `category_id`, `product_name`, `image`, `sellingPrice`, `description`, `quantity`) VALUES
 (1, 1, 'dark red fabric scrunchy', 'dark_red_fabric_scrunchy.jpg', 5.99, '    Made out of dark red handpicked fabric, sure to give wearer a nice look with low price    ', 7),
 (2, 3, 'pink Rojin keychain', '644842a3e269a.png', 3.99, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laor', 3),
-(11, 1, 'Purple scrunchy', '6448680577893.png', 5.99, 'The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn\'t distract ', 4);
+(11, 1, 'Purple scrunchy', '6448680577893.png', 5.99, 'The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn\'t distract ', 4),
+(14, 2, 'braid 123', '645ba11005883.jpg', 4.99, '  The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfe ', 5),
+(15, 4, 'tote bag', '645ba5eae8176.jpg', 12.99, 'eragasreaera', 3);
 
 -- --------------------------------------------------------
 
@@ -138,7 +165,7 @@ CREATE TABLE `profile` (
   `city` varchar(50) NOT NULL,
   `province` varchar(50) NOT NULL,
   `postal` varchar(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `profile`
@@ -147,7 +174,10 @@ CREATE TABLE `profile` (
 INSERT INTO `profile` (`user_id`, `subscription`, `name`, `phoneNo`, `address`, `city`, `province`, `postal`) VALUES
 (9, 0, 'Jojo', '524-324-32', '123 haha avenue', 'Montreal', 'Quebec', 'T6J 7W3'),
 (10, 0, 'abc', '1029384756', '123 fakeStreet', 'Montreal', 'Quebec', 'a1a 2b2'),
-(11, 0, 'eto', '1029384756', '123 fakeStreet', 'Montreal', 'Quebec', 'a1a 2b2');
+(11, 0, 'eto', '1029384756', '123 fakeStreet', 'Montreal', 'Quebec', 'a1a 2b2'),
+(12, 0, 'hello', '1234567890', '123 hello street', 'Montreal', 'Qc', 'T6C 2H8'),
+(13, 0, 'cia', '0012346578', '00 fakestreet ', 'montreal', 'Quebec', 'b2b 1a1'),
+(14, 1, 'hi', '5354566789', '123 haha street', 'Montreal', 'Qc', 'H4T Y6U');
 
 -- --------------------------------------------------------
 
@@ -161,7 +191,7 @@ CREATE TABLE `user` (
   `email` varchar(50) NOT NULL,
   `password` varchar(72) NOT NULL,
   `roleType` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
@@ -170,9 +200,12 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`user_id`, `email`, `password`, `roleType`) VALUES
 (1, 'TestUser', '$2y$10$cA44WNtp7qhpgt2OsPuz/eGZlT.ketAfmu5cPBoZOVfupqH1/P1G.', 'customer'),
 (6, 'TestUser2', '$2y$10$litEQ/Cl5hIZPyAWFAc/DOXUOuiAB53LlpFrD63VtLX7CCwrN6unK', 'customer'),
-(9, 'TestUser4', '$2y$10$8tEuaTguZitcslQwBkaVDus8nrMq921J4kdD7YKl.8BgsW2xbnKGy', 'customer'),
+(9, 'TestUser4@email.com', '$2y$10$8tEuaTguZitcslQwBkaVDus8nrMq921J4kdD7YKl.8BgsW2xbnKGy', 'customer'),
 (10, 'abc@abc.com', '$2y$10$kBfo8N.XaTnCdQsGJIEcxuecaLl54tzYxQFo5cj/.GkqLrp1qrDky', 'customer'),
-(11, 'eto@eto.com', '$2y$10$DCLqcAKIsJGM.aml1f33Ue7cIZy3m9xNNXuGsYxg8jgJyZrMFfbUK', 'admin');
+(11, 'eto@eto.com', '$2y$10$DCLqcAKIsJGM.aml1f33Ue7cIZy3m9xNNXuGsYxg8jgJyZrMFfbUK', 'admin'),
+(12, 'hello@email.com', '$2y$10$.IqeWzWFaumjV06NFXtBwuXYjwK0GTfv0zuliVWHReJuB5bBPeC/u', 'customer'),
+(13, 'bca@bca.com', '$2y$10$WgCZFxUUHPkwsReQgt.YbunuVAZfpfrWD7DBKCi6nFIiXUSo5u2de', 'customer'),
+(14, 'hi@email.com', '$2y$10$iKosmbOKpHx94vzsGhvaweeEhr6UrUNMlBGZda8AWZ49c5n68iX1.', 'customer');
 
 --
 -- Indexes for dumped tables
@@ -243,25 +276,25 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `detail`
 --
 ALTER TABLE `detail`
-  MODIFY `detail_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `detail_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `order_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `order_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `product_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `user_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
