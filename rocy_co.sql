8f0b1bdac1fd9a70cf5df20883957982ccc387f3
DokuSan00 <kanekike12@gmail.com>
2023-05-10 09:18:13 -0400

    forgot to push new sql

--- rocy_co.sql
+++ rocy_co.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2023 at 08:26 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: May 10, 2023 at 03:16 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,19 +30,18 @@ USE `rocy_co`;
 --
 
 DROP TABLE IF EXISTS `about_us`;
-CREATE TABLE IF NOT EXISTS `about_us` (
-  `about_us_Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `about_us` (
+  `about_us_Id` int(11) NOT NULL,
   `image` varchar(72) NOT NULL,
-  `text` text NOT NULL,
-  PRIMARY KEY (`about_us_Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `about_us`
 --
 
 INSERT INTO `about_us` (`about_us_Id`, `image`, `text`) VALUES
-(1, '645869acc9dba.png', 'THIS WORKS FINALLYYYYY');
+(1, '645869acc9dba.png', 'The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn\'t distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.\r\n\r\nThe passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it\'s seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.');
 
 -- --------------------------------------------------------
 
@@ -51,11 +50,10 @@ INSERT INTO `about_us` (`about_us_Id`, `image`, `text`) VALUES
 --
 
 DROP TABLE IF EXISTS `category`;
-CREATE TABLE IF NOT EXISTS `category` (
-  `category_id` int(2) NOT NULL AUTO_INCREMENT,
-  `category` varchar(50) NOT NULL,
-  PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `category` (
+  `category_id` int(2) NOT NULL,
+  `category` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `category`
@@ -74,31 +72,13 @@ INSERT INTO `category` (`category_id`, `category`) VALUES
 --
 
 DROP TABLE IF EXISTS `detail`;
-CREATE TABLE IF NOT EXISTS `detail` (
-  `detail_id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `detail` (
+  `detail_id` int(5) NOT NULL,
   `order_id` int(5) NOT NULL,
   `product_id` int(5) NOT NULL,
   `unit_price` float NOT NULL,
-  `quantity` int(3) NOT NULL,
-  PRIMARY KEY (`detail_id`),
-  KEY `order_id_detail_to_order` (`order_id`),
-  KEY `product_id_detail_to_product` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `detail`
---
-
-INSERT INTO `detail` (`detail_id`, `order_id`, `product_id`, `unit_price`, `quantity`) VALUES
-(5, 5, 1, 5.99, 2),
-(6, 5, 3, 7.99, 1),
-(9, 9, 1, 5.99, 1),
-(10, 10, 3, 7.99, 1),
-(11, 11, 1, 5.99, 1),
-(12, 12, 1, 5.99, 1),
-(16, 13, 2, 3.99, 1),
-(17, 13, 3, 7.99, 1),
-(19, 15, 1, 5.99, 1);
+  `quantity` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -107,30 +87,14 @@ INSERT INTO `detail` (`detail_id`, `order_id`, `product_id`, `unit_price`, `quan
 --
 
 DROP TABLE IF EXISTS `order`;
-CREATE TABLE IF NOT EXISTS `order` (
-  `order_id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `order` (
+  `order_id` int(5) NOT NULL,
   `user_id` int(4) NOT NULL,
   `order_date` date NOT NULL DEFAULT current_timestamp(),
   `status` enum('cart','ordered','finished') DEFAULT NULL,
   `total_price` float NOT NULL,
-  `isDelivery` tinyint(1) NOT NULL,
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `order`
---
-
-INSERT INTO `order` (`order_id`, `user_id`, `order_date`, `status`, `total_price`, `isDelivery`) VALUES
-(5, 10, '2023-05-05', 'ordered', 5.5, 0),
-(9, 10, '2023-05-06', 'ordered', 0, 0),
-(10, 10, '2023-05-06', 'ordered', 0, 0),
-(11, 10, '2023-05-06', 'ordered', 1, 0),
-(12, 10, '2023-05-06', 'ordered', 2, 1),
-(13, 10, '2023-05-07', 'ordered', 11.98, 1),
-(14, 10, '2023-05-07', 'cart', 0, 0),
-(15, 11, '2023-05-07', 'ordered', 5.99, 1),
-(16, 11, '2023-05-07', 'cart', 0, 0);
+  `isDelivery` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -139,17 +103,15 @@ INSERT INTO `order` (`order_id`, `user_id`, `order_date`, `status`, `total_price
 --
 
 DROP TABLE IF EXISTS `product`;
-CREATE TABLE IF NOT EXISTS `product` (
-  `product_id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product` (
+  `product_id` int(5) NOT NULL,
   `category_id` int(2) NOT NULL,
   `product_name` varchar(100) NOT NULL,
   `image` varchar(72) NOT NULL,
   `sellingPrice` float NOT NULL,
   `description` varchar(128) NOT NULL,
-  `quantity` int(3) NOT NULL,
-  PRIMARY KEY (`product_id`),
-  KEY `category_id_product_to_category` (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `quantity` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `product`
@@ -157,10 +119,8 @@ CREATE TABLE IF NOT EXISTS `product` (
 
 INSERT INTO `product` (`product_id`, `category_id`, `product_name`, `image`, `sellingPrice`, `description`, `quantity`) VALUES
 (1, 1, 'dark red fabric scrunchy', 'dark_red_fabric_scrunchy.jpg', 5.99, '    Made out of dark red handpicked fabric, sure to give wearer a nice look with low price    ', 7),
-(2, 3, 'pink Rojin keychain', '644842a3e269a.png', 3.99, '               for test               ', 3),
-(3, 1, 'TestProduct', '6447e2446ce77.png', 7.99, '       This is a test product.       ', 4),
-(10, 3, 'ProductTest', '6448b77922ca6.png', 5.99, '   hehehehhe         ', 5),
-(11, 1, 'Purple scrunchy', '6448680577893.png', 5.99, ' This is a test. ', 4);
+(2, 3, 'pink Rojin keychain', '644842a3e269a.png', 3.99, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laor', 3),
+(11, 1, 'Purple scrunchy', '6448680577893.png', 5.99, 'The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn\'t distract ', 4);
 
 -- --------------------------------------------------------
 
@@ -169,7 +129,7 @@ INSERT INTO `product` (`product_id`, `category_id`, `product_name`, `image`, `se
 --
 
 DROP TABLE IF EXISTS `profile`;
-CREATE TABLE IF NOT EXISTS `profile` (
+CREATE TABLE `profile` (
   `user_id` int(4) NOT NULL,
   `subscription` tinyint(1) NOT NULL,
   `name` varchar(50) NOT NULL,
@@ -177,9 +137,8 @@ CREATE TABLE IF NOT EXISTS `profile` (
   `address` varchar(50) NOT NULL,
   `city` varchar(50) NOT NULL,
   `province` varchar(50) NOT NULL,
-  `postal` varchar(7) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `postal` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `profile`
@@ -197,13 +156,12 @@ INSERT INTO `profile` (`user_id`, `subscription`, `name`, `phoneNo`, `address`, 
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(4) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `user_id` int(4) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(72) NOT NULL,
-  `roleType` varchar(10) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `roleType` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
@@ -217,6 +175,95 @@ INSERT INTO `user` (`user_id`, `email`, `password`, `roleType`) VALUES
 (11, 'eto@eto.com', '$2y$10$DCLqcAKIsJGM.aml1f33Ue7cIZy3m9xNNXuGsYxg8jgJyZrMFfbUK', 'admin');
 
 --
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about_us`
+--
+ALTER TABLE `about_us`
+  ADD PRIMARY KEY (`about_us_Id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `detail`
+--
+ALTER TABLE `detail`
+  ADD PRIMARY KEY (`detail_id`),
+  ADD KEY `order_id_detail_to_order` (`order_id`),
+  ADD KEY `product_id_detail_to_product` (`product_id`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `category_id_product_to_category` (`category_id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `about_us`
+--
+ALTER TABLE `about_us`
+  MODIFY `about_us_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `detail`
+--
+ALTER TABLE `detail`
+  MODIFY `detail_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `order_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -225,7 +272,7 @@ INSERT INTO `user` (`user_id`, `email`, `password`, `roleType`) VALUES
 --
 ALTER TABLE `detail`
   ADD CONSTRAINT `order_id_detail_to_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `product_id_detail_to_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
+  ADD CONSTRAINT `product_id_detail_to_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `product`
